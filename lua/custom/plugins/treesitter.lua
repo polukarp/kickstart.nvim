3d776c009ec79b4ec12c44b270165c6042bd506e
Daniel <perzhul.works@gmail.com>
2024-03-06 12:45:22 +0300

    feat: add autopairs

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -13,7 +13,9 @@ return {
       indent = { enable = true },
       -- Install parsers synchronously (only applied to `ensure_installed`)
       sync_install = false,
-
+      autotag = {
+        enable = true,
+      },
       -- Automatically install missing parsers when entering buffer
       -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
       auto_install = true,
