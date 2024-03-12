82a50b512949028bb00cc435f86d91368bd89dab
Danil Perzhul <perzhul.works@gmail.com>
2024-03-12 08:02:59 +0300

    feat: add a bunch of language servers

--- init.lua
+++ init.lua
@@ -414,28 +414,18 @@ require('lazy').setup {
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
-      -- Enable the following language servers
-      --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-      --
-      --  Add any additional override configuration in the following tables. Available keys are:
-      --  - cmd (table): Override the default command used to start the server
-      --  - filetypes (table): Override the default list of associated filetypes for the server
-      --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-      --  - settings (table): Override the default settings passed when initializing the server.
-      --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         clangd = {},
         gopls = {},
         pyright = {},
         rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`tsserver`) will work just fine
+        ansiblels = {},
+        dockerls = {},
+        tailwindcss = {},
+        prettierd = {},
+        bashls = {},
         tsserver = {},
-        eslint = {},
+        eslint_d = {},
         cssls = {},
         lua_ls = {
           settings = {
@@ -475,6 +465,13 @@ require('lazy').setup {
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format lua code
+        'dockerfile-language-server',
+        'typescript-language-server',
+        'prettierd',
+        'css-lsp',
+        'eslint_d',
+        'bash-language-server',
+        'dockerfile-language-server',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
