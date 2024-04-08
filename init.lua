8e565f1ea061ec1e39b49f32bfdf830bcb5d876c
polukarp <perzhul.works@gmail.com>
2024-04-08 10:34:05 +0300

    fix: wrong repository name for LuaSnip

--- init.lua
+++ init.lua
@@ -400,11 +400,8 @@ require('lazy').setup {
     dependencies = {
       -- Snippet Engine & its associated nvim-cmp source
       {
-        'L4MON4D3/LuaSnip',
+        'L3MON4D3/LuaSnip',
         build = (function()
-          -- Build Step is needed for regex support in snippets
-          -- This step is not supported in many windows environments
-          -- Remove the below condition to re-enable on windows
           if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
             return
           end
